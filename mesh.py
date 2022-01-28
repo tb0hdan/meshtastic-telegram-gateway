0d3b4ae6aa66507e91b0423a0bc9f56a435ee82e
Bohdan Turkynewych <tb0hdan@gmail.com>
2022-01-28 04:17:42 +0200

    main

--- mesh.py
+++ mesh.py
@@ -1022,7 +1022,7 @@ class WebServer:  # pylint:disable=too-few-public-methods
         self.server.shutdown()
 
 
-if __name__ == '__main__':
+def main():
     config = Config()
     config.read()
     level = logging.INFO
@@ -1055,3 +1055,6 @@ if __name__ == '__main__':
         web_server.shutdown()
         logger.info('Exit requested...')
         sys.exit(0)
+
+if __name__ == '__main__':
+    main()
