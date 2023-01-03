838f82d32949fe4ad1184958073bb6cb579c5177
Bohdan Turkynewych <tb0hdan@gmail.com>
2023-01-03 23:54:19 +0200

    Added SNR to CSVFileWriter

--- mtg/output/file/csv.py
+++ mtg/output/file/csv.py
@@ -10,11 +10,14 @@ class CSVFileWriter:
         self.logger = logger
 
     def write(self, packet):
-        position = packet.get('decoded', {}).get('position', {})
+        decoded = packet.get('decoded', {})
+        position = decoded.get('position', {})
+        # local node
+        snr = packet.get('rxSnr', 10)
         latitude = position.get('latitude')
         longitude = position.get('longitude')
         rx_time = time.strftime('%d.%m.%y %H:%M:%S')
-        fields = [rx_time, packet.get('fromId'), packet.get('toId'), latitude, longitude]
+        fields = [rx_time, packet.get('fromId'), packet.get('toId'), snr, latitude, longitude]
         with open(self.dst, 'a') as f:
             writer = csv.writer(f)
             writer.writerow(fields)
