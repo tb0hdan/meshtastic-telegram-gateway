1fc62a2a83c67cb5274e409685f8a95e7e39ec09
Bohdan Turkynewych <tb0hdan@gmail.com>
2023-03-07 23:31:46 +0200

    Added simple monitor script

---
+++ monitor.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# slow startup
+sleep 10
+
+# run monitoring
+while :; do
+    wget --timeout 3 -t 1 http://localhost:5000 -O /tmp/mtg.html >/dev/null 2>/dev/null
+    if [ $? -ne 0 ]; then
+        echo 'ERR!'
+        mesh_pid=$(ps ax|grep 'python3 ./mesh.py'|grep -v 'grep'|awk '{print $1}')
+        if [ "${mesh_pid}" != "" ]; then
+            kill ${mesh_pid}
+        fi
+        acm_pid=$(lsof -n|grep ACM|awk '{print $2}')
+        if [ "${acm_pid}" != "" ]; then
+            kill -9 ${acm_pid}
+        fi
+    fi
+    sleep 60
+done
--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@
 cd $(dirname $0)
 
 screen -t MESH -S MESH -d -m sh -c 'make run >>logfile.out 2>>logfile.err'
+screen -t MESH-MON -S MESH-MON -d -m sh -c './monitor.sh'
 
