f7417fcafbc9bd197f0de041d9295177277356e2
Darafei Praliaskouski <me@komzpa.net>
2025-11-05 16:32:47 +0200

    Add Telegram reaction forwarding support

--- mtg/connection/telegram/__init__.py
+++ mtg/connection/telegram/__init__.py
@@ -1,4 +1,11 @@
 # -*- coding: utf-8 -*-
 """ Telegram connection module """
 
+from .reaction import MessageReactionHandler, ensure_reaction_update_support
 from .telegram import TelegramConnection
+
+__all__ = [
+    'MessageReactionHandler',
+    'TelegramConnection',
+    'ensure_reaction_update_support',
+]
